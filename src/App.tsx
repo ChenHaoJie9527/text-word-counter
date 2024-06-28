4ebbd49871b8022d920eb602ea11d91573ea7163
ChenHaoJie9527 <a18520145493@163.com>
2024-06-28 22:54:36 +0800

    change textarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import Counter from "./components/Counter";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import { Textarea } from "./components/ui/textarea";
 function App() {
+  const [textValue, setTextValue] = useState("");
+
+  const onChangeValue = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const val = e.target.value;
+    setTextValue(val);
+  };
   return (
     <div className="flex flex-col w-full h-full relative">
       <Header />
       <Main>
-        <Textarea placeholder="Type your message here." className="rounded-none border-none"  />
-        <Counter className="flex-[40%]" />
+        <Textarea
+          value={textValue}
+          onChange={onChangeValue}
+          placeholder="Type your message here."
+          className="rounded-none border-none"
+        />
+        <Counter className=" xl:flex-[40%] md:flex-[40%] sm:flex-[55%]" />
       </Main>
       <Footer />
     </div>
