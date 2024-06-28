ac1eca44d031f1ebfd4dd10da64df25da66586a1
ChenHaoJie9527 <a18520145493@163.com>
2024-06-28 15:25:19 +0800

    container 阴影

--- src/components/base/Container.tsx
+++ src/components/base/Container.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 export default function Container({ children }: Props) {
   return (
-    <main className="min-w-[1080px] min-h-[520px] bg-[#ffffff] rounded-md shadow-md flex mt-[55px] overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <main className="min-w-[1080px] min-h-[520px] bg-[#ffffff] rounded-md shadow-xl flex mt-[55px] overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
       {children}
     </main>
   );
